fc110f392048f49214a97dc967c44713e30c9671
Indu1234 <86624356+Indu1234@users.noreply.github.com>
2023-08-26 12:54:59 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,9 @@
+# Create VPC
 resource "aws_vpc" "k8s_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+# create Subnet
 resource "aws_subnet" "nodepool_subnet" {
   vpc_id     = aws_vpc.k8s_vpc.id
   cidr_block = "10.0.1.0/24"
