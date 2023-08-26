dca46b6674e5775fce462df2292e4342134e0f9a
Indu1234 <86624356+Indu1234@users.noreply.github.com>
2023-08-26 12:55:39 +0530

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+locals {
+  instance_names = ["k8s-master", "k8s-node1", "k8s-node2"]
+}
+
+resource "aws_instance" "myec2vm" {
+  count = length(local.instance_names)
+
+  ami                    = data.aws_ami.Ubuntu.id # Using dynamic ami id for Ubuntu LTS 22.04
+  instance_type          = var.instance_type
+  key_name               = var.instance_keypair
+  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
+  tags = {
+    Name = local.instance_names[count.index]
+  }
+}
